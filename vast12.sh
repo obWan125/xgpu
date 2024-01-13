6fdcf98e459ef76d48c7d1a1a3bbaee8cadce4cb
obWan125 <156277768+obWan125@users.noreply.github.com>
2024-01-13 14:48:07 -0600

    Update vast12.sh
    Added my XN2themoonwallet

--- vast12.sh
+++ vast12.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x295388d23D23c8520A7B4845a8782080294fBB31/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xc1Db432fE0a27B0BBa66040d68F60EDdD63D311a/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
